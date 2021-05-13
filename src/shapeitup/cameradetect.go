396318d8ceda875e7b2a1c2868285c42f850a913
Dexter Leander <dexter@MacBook-Pro-som-tillhor-Dexter.local>
2021-05-13 14:01:32 +0200

    Added documentation, moved BlurMat and ImageToGrayscaleMat and renamed calculatedistancebetweentwopoints

--- src/shapeitup/cameradetect.go
+++ src/shapeitup/cameradetect.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"image"
-
 	"gocv.io/x/gocv"
 	"shapeitup.com/helper"
 )
-
-// A Result represents a shape and a point where text should be written.
-type Result struct {
-	Shape     string      // The shape
-	Textpoint image.Point // The point where text is placed
-}
 
 func main() {
 	webcam, _ := gocv.VideoCaptureDevice(0)
@@ -20,7 +12,7 @@ func main() {
 
 	for {
 		webcam.Read(&img)
-		shapeimg, err := blurMat(img)
+		shapeimg, err := helper.BlurMat(img)
 		if err != nil {
 			break
 		}
@@ -32,13 +24,4 @@ func main() {
 			break
 		}
 	}
-}
-
-func blurMat(imgname gocv.Mat) (gocv.Mat, error) {
-	gocv.MedianBlur(imgname, &imgname, 11)
-	shapeimg := gocv.NewMat()
-	gocv.BilateralFilter(imgname, &shapeimg, 10, float64(100), float64(100))
-	//gocv.CvtColor(imgname, &shapeimg, gocv.ColorGrayToBGR)
-
-	return shapeimg, nil
 }
